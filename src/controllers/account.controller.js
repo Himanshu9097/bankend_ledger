3ebad558833426981f7433493aac8205211c5e8d
Himanshu9097 <himanshu748808@gmail.com>
2026-02-14 21:04:13 +0530

    added create account : user can create account ( create middlewares , account routes controller )

---
+++ src/controllers/account.controller.js
@@ -0,0 +1,17 @@
+const accountModel = require("../models/account.model");
+
+async function createAccountController(req,res){
+    const userId = req.user;
+
+    const account = await accountModel.create({
+        user:userId._id
+    })
+
+    res.status(201).json({
+        account
+    })
+}
+
+module.exports = {
+    createAccountController
+}
---
+++ src/models/account.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:[true,"Account must be associated with a user"],
+        index:true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["ACTIVE", "FROZEN", "CLOSED"],
+            message: "{VALUE} is not a valid status"
+        },
+        default: "ACTIVE"
+    },
+    currency:{
+        type:String,
+        required:[true,"currency is required for creating an account"],
+        default:"INR"
+    }
+},{
+    timestamps:true
+})
+
+accountSchema.index({user:1,status:1})
+
+
+const accountModel = mongoose.model("account",accountSchema);
+
+module.exports = accountModel
