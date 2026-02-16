795b23a1e3443a2ee2f936e210a0e9c7f3967bcb
Himanshu9097 <himanshu748808@gmail.com>
2026-02-16 23:14:18 +0530

    update the transaction controller using get balance method valide the account

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -4,7 +4,11 @@ const accountModel = require("../models/account.model");
 const emailService = require("../services/email.service");
 
 
-async function createTransactionController(req,res){
+async function createTransaction(req,res){
+
+    /**
+     * validate request
+     */
 
     const {fromAccount,toAccount,amount,idempotencyKey} = req.body;
 
@@ -14,4 +18,78 @@ async function createTransactionController(req,res){
         })
     }
 
+    const fromUserAccount = await accountModel.findOne({
+        _id:fromAccount,
+    })
+
+    const toUserAccount = await accountModel.findOne({
+        _id:toAccount,
+    })
+
+    if(!fromUserAccount || !toUserAccount){
+        return res.status(404).json({
+            message:"fromAccount or toAccount not found"
+        })
+    }
+    
+    /**
+     * Validate idempotency key
+     */
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey
+    })
+
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists.status === "SUCCESS"){
+            return res.status(200).json({
+                message:"Transaction already completed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === "PENDING"){
+            return res.status(200).json({
+                message:"Transaction is pending",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === "FAILED"){
+            return res.status(400).json({
+                message:"Transaction failed, please try again",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === "REVERSED"){
+            return res.status(400).json({
+                message:"Transaction is reversed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+    }
+
+    /**
+     * check account status
+     */
+
+    if(fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE"){
+        return res.status(400).json({
+            message:"fromAccount or toAccount is not active"
+        })
+    }
+
+    /**
+     * check balance
+     */
+
+    const balance = await fromUserAccount.getBalance();
+
+    if(balance < amount){
+        return res.status(400).json({
+            message:`Insufficient balance in fromAccount. Current balance is ${balance}`
+        })
+    }
 }
